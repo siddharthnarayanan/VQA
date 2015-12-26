fd1061e8e05d7fe2249c16492e38a9d76c3cc9e3
siddharthnarayanan <siddharth.narayanan@ymail.com>
2015-12-26 16:16:47 -0500

    Create train_img_qa.bat

---
+++ train_img_qa.bat
@@ -0,0 +1,21 @@
+md dssm
+md dssm\model
+
+REM shuffle the training data
+REM ..\bin\WordHash.exe --shuffle data\train.pair.tok.tsv data\train.sf.tsv
+
+REM generate features
+REM ..\bin\WordHash.exe --pair2seqfea data\train.pair.tok.tsv data\l3g.txt data\l3g.txt 1 dssm\train
+
+REM convert feature files into binary files
+..\bin\WordHash.exe --seqfea2bin  dssm\train.ans_plus_ques.src.fea.full 1024 dssm\train.ans_plus_ques.src.fea.full.bin
+..\bin\WordHash.exe --seqfea2bin  dssm\train.img.src.fea.full 1024 dssm\train.img.src.fea.full.bin
+
+REM compute "noise distribution" which is approximated by scaled unigram distribution for NCE training.
+REM ..\bin\ComputelogPD.exe /i data\train.pair.tok.tsv /o mix\train.logpD.s75 /C 1 /S 0.75
+
+REM cdssm training
+..\bin\DSSM_Train.exe I_QA.config.txt
+
+REM perform sent2vec using the trained model to dev set
+REM ..\bin\sent2vec /inSrcModel dssm\model\dssm_QUERY_DONE /inSrcVocab data\l3g.txt /inSrcModelType DSSM /inTgtModel dssm\model\dssm_DOC_DONE /inTgtVocab data\l3g.txt /inTgtModelType DSSM /inFilename data\dev.pair.tok.tsv /outFilenamePrefix dssm\dev.sent2vec
